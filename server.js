4735b41c574c35dcd0cd41b04a6d11fbf430473f
Ahmed Awadelkarim <ahmed.awadelkarim@edfundrgiving.com>
2016-06-14 11:09:25 -0400

    Static Todos

--- server.js
+++ server.js
@@ -9,14 +9,31 @@ var todoNextID = 1;
 
 app.use(bodyParser.json());
 
+//GET /todos?completed=true
 app.get('/', function (req, res) {
 	res.send('TODO API Root');
 });
 
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = .filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase) > -1;
+		});
+	}
+	res.json(filteredTodos);
+
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoID = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoID});
@@ -53,6 +70,37 @@ app.delete('/todos/:id,', function (req, res) {
 		res.json(matchedTodo);
 	}
 	
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoID});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed) ) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} else {
+		console.log('No valid attribute provided');
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length === 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	} else {
+		console.log('No valid attribute provided');
+	}
+	
+	_.extend(matchedTodo, validAttributes);
+	res.json(validAttributes);
 });
 
 app.listen(PORT, function () {
