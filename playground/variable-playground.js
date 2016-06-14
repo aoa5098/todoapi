163cc1a41683ee85fcf0afbdbc9519486024b932
Ahmed Awadelkarim <ahmed.awadelkarim@edfundrgiving.com>
2016-06-14 15:21:53 -0400

    Add files

---
+++ playground/variable-playground.js
@@ -0,0 +1,28 @@
+//var person = {
+//	name: 'Ahmed',
+//	age: '21'
+//};
+
+//function updatePerson (obj) {
+//	obj = {
+//		name: 'Ahmed',
+//		age: '27'
+//	};
+//	obj.age = 24;
+//}
+
+//updatePerson(person);
+//console.log(person);
+
+//Array Example
+
+var grades = [15, 37];
+
+function addGrade (gradesArr) {
+	grades.push(100);
+	debugger;
+	//gradesArr = [12, 33, 99];
+}
+
+addGrade(grades);
+console.log(grades);
